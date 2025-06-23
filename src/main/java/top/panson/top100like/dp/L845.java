b96c7f7d1ffe5f66344349337b6d69890eff5a1f
Panson <PansonPanson@163.com>
2025-06-23 21:08:32 +0800

    add solution

--- src/main/java/top/panson/top100like/dp/L845.java
+++ src/main/java/top/panson/top100like/dp/L845.java
@@ -30,4 +30,36 @@ public class L845 {
             return maxLength;
         }
     }
+
+
+
+    class Solution1 {
+        public int longestMountain(int[] arr) {
+            int n = arr.length;
+            int maxLength = 0;
+            int i = 1;
+            while(i < n - 1) {
+
+                if(arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) {
+                    int left = i;
+                    int right = i;
+                    int curLength = 1;
+
+                    while(left - 1 >= 0 && arr[left] > arr[left - 1] ) {
+                        left--;
+                        curLength++;
+                    }
+                    while(right + 1 < n && arr[right] > arr[right + 1]) {
+                        right++;
+                        curLength++;
+                    }
+                    maxLength = Math.max(maxLength, curLength);
+                    i = right;
+                } else {
+                    i++;
+                }
+            }
+            return maxLength;
+        }
+    }
 }

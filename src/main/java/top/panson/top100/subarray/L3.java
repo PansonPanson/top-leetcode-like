638997de95fbf7871e4df1fa0227c2be914db422
Panson <PansonPanson@163.com>
2025-07-19 17:49:36 +0800

    add solution

---
+++ src/main/java/top/panson/top100/subarray/L3.java
@@ -0,0 +1,33 @@
+package top.panson.top100.subarray;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Panson
+ * @create 2025-07-19
+ */
+public class L3 {
+
+    class Solution {
+        public int lengthOfLongestSubstring(String s) {
+            // 双指针
+            Map<Character, Integer> map = new HashMap<>();
+            int left = 0;
+            int right = 0;
+            int maxLength = 0;
+            for(int i = 0; i < s.length(); i++) {
+                if(!map.containsKey(s.charAt(i))) {
+                    map.put(s.charAt(i), i);
+                } else {
+                    int preIndex = map.get(s.charAt(i));
+                    map.put(s.charAt(i), i);
+                    // dvdf
+                    left = Math.max(left, preIndex + 1);
+                }
+                maxLength = Math.max(maxLength, i - left + 1);
+            }
+            return maxLength;
+        }
+    }
+}

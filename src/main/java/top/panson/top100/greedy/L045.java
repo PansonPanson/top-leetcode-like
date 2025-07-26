47c3f565d4fa44e50dbbea1dba0844c68166ce6c
Panson <PansonPanson@163.com>
2025-07-26 22:36:56 +0800

    add solution

--- src/main/java/top/panson/top100/greedy/L045.java
+++ src/main/java/top/panson/top100/greedy/L045.java
@@ -9,7 +9,9 @@ public class L045 {
         public int jump(int[] nums) {
             // 跳第一个，设置最远边界1，在这个边界里遍历，记录所能达到的最远距离2，更新2，到达最远边界 step+1
             int step = 0;
-            int end = 0;
+            // 当前右边界
+            int windowEnd = 0;
+            // 维护下一次右边界
             int maxReach = 0;
             int n = nums.length;
             for(int i = 0; i < n; i++) {
@@ -17,9 +19,9 @@ public class L045 {
                     maxReach = i + nums[i];
                 }
 
-                if(i == end && i < n - 1) {
+                if(i == windowEnd && i < n - 1) {
                     step++;
-                    end = maxReach;
+                    windowEnd = maxReach;
                 }
             }
             return step;
